e4c7bb35e0c2d8341bbc012bd3bfdec4706b670b
sske0 <akbaraz@code.edu.az>
2023-04-20 02:25:12 +0400

    initial

---
+++ main.js
@@ -0,0 +1,33 @@
+// String.prototype.customCapitalize = function() {
+//     let result = this.charAt(0).toUpperCase() + this.slice(1);
+//     return result;
+// };
+
+// let str = "hello";
+// console.log(str.customCapitalize());
+
+// String.prototype.customReverse = function() {
+//     let result = '';
+//     for (let i = this.length - 1; i >= 0; i--) {
+//         result += this[i];
+//     }
+//     return result;
+// }
+
+// let str = "bruh";
+// console.log(str.customReverse());
+
+String.prototype.customVowelCapitalize = function () {
+  let result = "";
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] == "a" || this[i] == "e" || this[i] == "u" || this[i] == "i" || this[i] == "o") {
+      result += this[i].toUpperCase();
+    } else {
+      result += this[i];
+    }
+  }
+  return result;
+};
+
+let str = "words with vowels: some random words. P.S. sorry for doing that shit while shaking hands, it's not some silly joke, ive just felt kinda nervous and it happened on its own";
+console.log(str.customVowelCapitalize());
